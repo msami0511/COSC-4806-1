6afacbe3f66b0c65a05c90341dc14b367a4d6e64
msami0511 <mohamashaikh@algomau.ca>
2025-05-22 18:44:30 +0000

    Failed attempt sessions and redirect added

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+session_start();
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] == false){
+  header('Location: /login.php');
+  exit();
+}
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -6,6 +13,7 @@
   
   <body>
       <h1>Assignment 1</h1>
+      <p>Welcome <?php echo $_SESSION['username'] ?></p>
       <p><a href="/login.php"> Click to login</a></p>
     
   </body>
--- validate.php
+++ validate.php
@@ -1,14 +1,26 @@
 <?php
 
+   session_start();
+
   $valid_username = "MohamadSami";
   $valid_password = "password";
 
   $username = $_REQUEST['username'];
+  $_SESSION['username'] = $username;
   $password = $_REQUEST['password'];
 
-  if ($username == $valid_username && $password == $valid_password) {
-    echo "Login successful!";
-  } else {
-    echo "Invalid username or password.";
+  if ($valid_username == $username && $valid_password == $password) {
+    $_SESSION['authenticated'] = true;
+    header('Location: /');
+    } else {
+  
+    if (!isset($_SESSION['failed_attempts'])){
+      $_SESSION['failed_attempts'] = 1;
+    } else {
+      $_SESSION['failed_attempts'] = $_SESSION['failed_attempts'] + 1;
+    }
+    echo "Unsuccessful login attempt number: " . $_SESSION['failed_attempts'];
   }
+  
+  
   ?>
